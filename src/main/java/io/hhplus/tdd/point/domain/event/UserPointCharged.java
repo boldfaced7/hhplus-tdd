f12ac92c6937f775d369001eb599851ba1187bb7
boldfaced7 <boldfaced7@naver.com>
2025-05-20 13:17:34 +0900

    domain에 event 도입 - Event 클래스 작성 - UserPoint 상태 변경 시 이벤트 객체를 생성해 저장

---
+++ src/main/java/io/hhplus/tdd/point/domain/event/UserPointCharged.java
@@ -0,0 +1,15 @@
+package io.hhplus.tdd.point.domain.event;
+
+import io.hhplus.tdd.point.domain.enums.TransactionType;
+import io.hhplus.tdd.point.domain.vo.Amount;
+import io.hhplus.tdd.point.domain.vo.UserPointId;
+
+public record UserPointCharged(
+        UserPointId userPointId,
+        Amount amount
+) implements UserPointChanged {
+
+    public TransactionType transactionType() {
+        return TransactionType.CHARGE;
+    }
+}
--- src/main/java/io/hhplus/tdd/point/domain/model/UserPoint.java
+++ src/main/java/io/hhplus/tdd/point/domain/model/UserPoint.java
@@ -1,24 +1,44 @@
 package io.hhplus.tdd.point.domain.model;
 
+import io.hhplus.tdd.point.domain.event.UserPointChanged;
+import io.hhplus.tdd.point.domain.event.UserPointCharged;
+import io.hhplus.tdd.point.domain.event.UserPointUsed;
 import io.hhplus.tdd.point.domain.vo.Amount;
 import io.hhplus.tdd.point.domain.vo.Point;
 import io.hhplus.tdd.point.domain.vo.UpdateMillis;
 import io.hhplus.tdd.point.domain.vo.UserPointId;
+import lombok.Builder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public record UserPoint(
         UserPointId userPointId,
         Point point,
-        UpdateMillis updateMillis
+        UpdateMillis updateMillis,
+        List<UserPointChanged> userPointChangedList
 ) {
+
+    @Builder
+    public UserPoint(
+            UserPointId userPointId,
+            Point point,
+            UpdateMillis updateMillis
+    ) {
+        this(
+                userPointId,
+                point,
+                updateMillis,
+                new ArrayList<>()
+        );
+    }
 
     public static UserPoint of(
             Point point
     ) {
-        return new UserPoint(
-                null,
-                point,
-                null
-        );
+        return UserPoint.builder()
+                .point(point)
+                .build();
     }
 
     public static UserPoint of(
@@ -26,27 +46,54 @@ public record UserPoint(
             Point point,
             UpdateMillis updateMillis
     ) {
-        return new UserPoint(
-                userPointId,
-                point,
-                updateMillis
-        );
+        return UserPoint.builder()
+                .userPointId(userPointId)
+                .point(point)
+                .updateMillis(updateMillis)
+                .build();
+    }
+
+    public List<UserPointChanged> userPointChangedList() {
+        return List.copyOf(userPointChangedList);
+    }
+
+    public List<UserPointChanged> pullEvents() {
+        var events = List.copyOf(userPointChangedList);
+        userPointChangedList.clear();
+
+        return events;
     }
 
     public UserPoint chargePoint(Amount amount) {
-        return new UserPoint(
+        var charged = new UserPoint(
                 userPointId,
                 point.add(amount),
-                updateMillis
+                updateMillis,
+                userPointChangedList
         );
+        var raised = new UserPointCharged(
+                charged.userPointId,
+                amount
+        );
+        charged.userPointChangedList.add(raised);
+
+        return charged;
     }
 
     public UserPoint usePoint(Amount amount) {
-        return new UserPoint(
+        var used = new UserPoint(
                 userPointId,
                 point.subtract(amount),
-                updateMillis
+                updateMillis,
+                userPointChangedList
         );
+        var raised = new UserPointUsed(
+                used.userPointId,
+                amount
+        );
+        used.userPointChangedList.add(raised);
+
+        return used;
     }
 
 }
